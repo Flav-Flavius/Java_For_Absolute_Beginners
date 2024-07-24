44c1ff3e1065261d848f75367969a2f54173f834
Flavius Cenuse <olteaninardeal@gmail.com>
2024-07-24 10:08:04 +0300

    Update

---
+++ src/ForLoop/EvenNumber.java
@@ -0,0 +1,16 @@
+package ForLoop;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+        // Bucla for combinata cu instructiune if ce utilizezza operatorul modulo
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+
+        System.out.println("=====================");
+
+
+    }
+}
