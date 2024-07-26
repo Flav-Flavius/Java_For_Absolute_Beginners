2a1cad6bf934df04e05df2ebb2dd453a0901884a
Flavius Cenuse <olteaninardeal@gmail.com>
2024-07-26 16:44:04 +0300

    Update

---
+++ src/ForLoop/ArithmeticSequence.java
@@ -0,0 +1,16 @@
+
+// A program that prints the terms of the arithmetic sequence 1, 6, 11,...< 100
+package ForLoop;
+
+public class ArithmeticSequence {
+    public static void main(String[] args) {
+      for ( int i = 1; i < 100; i+= 5)
+          System.out.println(i);
+    }
+}
+
+
+
+/* O secventa aritmetica este un set ordonat de numere care au o diferenta comuna
+intre fiecare termen consecutiv.
+*/
---
+++ src/ForLoop/TableOfN.java
@@ -0,0 +1,18 @@
+
+// A program that takes an integer input by the user and prints a multiplication table
+package ForLoop;
+
+import java.util.Scanner;
+
+public class TableOfN {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int num = myObj.nextInt();
+        int result;
+        for (int i = 1; i < 11; i++) {
+            result = num * i;
+            System.out.println(num + " x " + i + " = " + result);
+        }
+    }
+}
